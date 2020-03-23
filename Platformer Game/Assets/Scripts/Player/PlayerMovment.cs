dbf7fc04ebfd8168eaa9fbaab4c399f2e7981f65
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-23 11:25:19 +0100

    28. moving player

---
+++ Platformer Game/Assets/Scripts/Player/PlayerMovment.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public float speed = 5f;
+
+    Rigidbody2D myBody;
+    Animator anim;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerWalk();
+    }
+
+    void PlayerWalk()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if(h > 0)
+        {
+            myBody.velocity = new Vector2(speed, myBody.velocity.y);
+        }
+        else if(h < 0)
+        {
+            myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+        }
+    }
+}
