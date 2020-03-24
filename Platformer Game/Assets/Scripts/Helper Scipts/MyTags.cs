7b58c243b311063bce683d7129cfe56466929171
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-24 14:18:14 +0100

    36

--- Platformer Game/Assets/Scripts/Helper Scipts/MyTags.cs
+++ Platformer Game/Assets/Scripts/Helper Scipts/MyTags.cs
@@ -8,4 +8,5 @@ public class MyTags : MonoBehaviour
 
 
     public static string BEETLE_TAG = "Beetle";
+    public static string SNAIL_TAG = "Snail";
 }
---
+++ Platformer Game/Assets/Scripts/Player/FireBullet.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    float speed = 10f;
+    Animator anim;
+
+    bool canMove;
+
+        private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        StartCoroutine(DisableBullet(5f));
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.x += speed * Time.deltaTime;
+            transform.position = temp;
+        }
+    }
+
+    public float Speed
+    {
+        get
+        {
+            return speed;
+        }
+        set
+        {
+            speed = value;
+        }
+    }
+
+    IEnumerator DisableBullet(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.tag == MyTags.BEETLE_TAG || target.gameObject.tag == MyTags.SNAIL_TAG)
+        {
+            anim.Play("Explode");
+            canMove = false;
+            StartCoroutine(DisableBullet(0.1f));
+        }
+    }
+
+}
